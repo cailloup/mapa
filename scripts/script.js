ffe2f21f57ac1e4debe1c3cf0396b78586cba6cf
cailloup <josevoyut@gmail.com>
2023-04-17 13:00:00 -0300

    se ve mejor en el celu

--- scripts/script.js
+++ scripts/script.js
@@ -1,60 +1,104 @@
-function zoom(event) {
-
-
-    scale += event.deltaY * -0.001;
-
-    //scale = Math.min(Math.max(0.125, scale), 4);
-
-    el.style.transform = `scale(${scale})`;
- }
-    
-
 let scale = 1;
 const el = document.getElementById("mapdiv");
 
+function zoom(event) {
+  if (event.type == "touchmove") {
+    event.preventDefault();
+    let finger1 = event.touches[0];
+    let finger2 = event.touches[1];
 
-document.onwheel = zoom;
-const body = document.getElementById("body")
-dragElement(document.getElementById("mapdiv"));
-function dragElement(elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    let distance = Math.sqrt(
+      Math.pow(finger2.clientX - finger1.clientX, 2) +
+      Math.pow(finger2.clientY - finger1.clientY, 2)
+    );
 
-    body.onmousedown = dragMouseDown;
+    let scaleValue = distance / 100;
 
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        body.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        body.onmousemove = elementDrag;
+    if (scaleValue > 0.25 && scaleValue < 4) {
+      scale = scaleValue;
     }
+  } else {
+    scale += event.deltaY * -0.001;
+  }
 
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-       // elmnt.style.left = Math.min(Math.max((elmnt.offsetLeft - pos1),-500),500) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement() {
-        // stop moving when mouse button is released:
-        body.onmouseup = null;
-        body.onmousemove = null;
-    }
-
-    
+  el.style.transform = `scale(${scale})`;
 }
 
+document.onwheel = zoom;
+document.addEventListener("touchmove", zoom, { passive: false });
+
+
+//move
+
+const mapSection = document.getElementById("map-section")
+var isTouchDevice = 'ontouchstart' in document.documentElement;
+dragElement(document.getElementById("mapdiv"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+  if (isTouchDevice) {
+    mapSection.addEventListener("touchstart", dragStart, { passive: false });
+    mapSection.addEventListener("touchend", dragEnd, { passive: false });
+    mapSection.addEventListener("touchmove", drag, { passive: false });
+  } else {
+    mapSection.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    mapSection.onmouseup = closeDragElement;
+
+    mapSection.onmousemove = elementDrag;
+  }
+
+  function dragStart(e) {
+    pos3 = e.touches[0].clientX;
+    pos4 = e.touches[0].clientY;
+  }
+
+  function drag(e) {
+    e.preventDefault();
+
+    pos1 = pos3 - e.touches[0].clientX;
+    pos2 = pos4 - e.touches[0].clientY;
+    pos3 = e.touches[0].clientX;
+    pos4 = e.touches[0].clientY;
+
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function dragEnd(e) {
+    mapSection.onmouseup = null;
+    mapSection.onmousemove = null;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    mapSection.onmouseup = null;
+    mapSection.onmousemove = null;
+  }
+}
+
+
+//hide
 function show(id,p){
     const elemento = document.querySelector("#"+id);
     elemento.classList.toggle("hide");
